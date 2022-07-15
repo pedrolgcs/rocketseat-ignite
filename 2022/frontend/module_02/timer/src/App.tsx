3e9a69e9c651443eea2895583ba7b6fc66549edf
Pedro Henrique <pedro.lg.cs@gmail.com>
2022-07-15 08:18:01 -0300

    feat: implement routing

--- 2022/frontend/module_02/timer/src/App.tsx
+++ 2022/frontend/module_02/timer/src/App.tsx
@@ -1,10 +1,15 @@
+import { BrowserRouter } from 'react-router-dom'
 import { ThemeProvider } from 'styled-components'
 import { GlobalStyle } from '@/styles/global'
+import { Router } from '@/routes/Router'
 import theme from '@/styles/themes/default'
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
       <GlobalStyle />
     </ThemeProvider>
   )
