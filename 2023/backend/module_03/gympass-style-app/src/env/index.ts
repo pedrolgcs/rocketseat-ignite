62a99250f678b60d06f9660b32d52c66bfb47678
Pedro Henrique <pedro.lg.cs@gmail.com>
2023-05-22 13:58:10 -0300

    chore: base structure

---
+++ 2023/backend/module_03/gympass-style-app/src/env/index.ts
@@ -0,0 +1,20 @@
+import 'dotenv/config'
+import { z } from 'zod'
+
+// schema validation
+const envSchema = z.object({
+  NODE_ENV: z.enum(['dev', 'production', 'test']).default('dev'),
+  APP_PORT: z.coerce.number().default(3333),
+})
+
+const _env = envSchema.safeParse(process.env)
+
+if (_env.success === false) {
+  console.error('Invalid environment variables', _env.error.format())
+
+  throw new Error('Invalid environment variables')
+}
+
+const env = _env.data
+
+export { env }
