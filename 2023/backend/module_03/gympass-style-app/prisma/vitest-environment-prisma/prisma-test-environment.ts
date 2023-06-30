5227b63a4bd9859582fb0d8d6fc0adffc3cabf9c
Pedro Henrique <pedro.lg.cs@gmail.com>
2023-06-30 09:08:34 -0300

    chore: configure prisma test envonriment

--- 2023/backend/module_03/gympass-style-app/prisma/vitest-environment-prisma/prisma-test-environment.ts
+++ 2023/backend/module_03/gympass-style-app/prisma/vitest-environment-prisma/prisma-test-environment.ts
@@ -1,13 +1,39 @@
+import 'dotenv/config'
+import { execSync } from 'node:child_process'
+import crypto from 'crypto'
 import { Environment } from 'vitest'
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
+
+function generateDatabaseURL(schema: string) {
+  if (!process.env.DATABASE_URL) {
+    throw new Error('Please provide a DATABASE_URL environment variable.')
+  }
+
+  const url = new URL(process.env.DATABASE_URL)
+
+  url.searchParams.set('schema', schema)
+
+  return url.toString()
+}
 
 export default <Environment>{
   name: 'prisma',
   async setup() {
-    console.log('setup')
+    const schema = crypto.randomUUID()
+    const databaseUrl = generateDatabaseURL(schema)
+
+    process.env.DATABASE_URL = databaseUrl
+
+    execSync('npx prisma migrate deploy')
 
     return {
-      teardown() {
-        console.log('teardown')
+      async teardown() {
+        await prisma.$executeRawUnsafe(
+          `DROP SCHEMA IF EXISTS "${schema}" CASCADE`,
+        )
+        await prisma.$disconnect()
       },
     }
   },
