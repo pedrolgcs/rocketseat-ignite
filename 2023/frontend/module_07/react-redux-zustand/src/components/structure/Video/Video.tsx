00d722bdbfb8515708a26a2114eecd7f4eabdece
Pedro Henrique <pedro.lg.cs@gmail.com>
2023-07-17 14:09:14 -0300

    feat: next video on end

--- 2023/frontend/module_07/react-redux-zustand/src/components/structure/Video/Video.tsx
+++ 2023/frontend/module_07/react-redux-zustand/src/components/structure/Video/Video.tsx
@@ -1,5 +1,7 @@
 import ReactPlayer from 'react-player'
+import { useDispatch } from 'react-redux'
 import { useAppSelector } from '@/store'
+import { actions } from '@/store/player'
 
 function Video() {
   const lesson = useAppSelector((state) => {
@@ -13,12 +15,20 @@ function Video() {
     return currentLesson
   })
 
+  const dispatch = useDispatch()
+
+  const handlePlayNext = () => {
+    dispatch(actions.next())
+  }
+
   return (
     <div className="w-full bg-zinc-950 aspect-video">
       <ReactPlayer
         width="100%"
         height="100%"
+        controls
         url={`https://www.youtube.com/watch?v=${lesson.id}`}
+        onEnded={handlePlayNext}
       />
     </div>
   )
