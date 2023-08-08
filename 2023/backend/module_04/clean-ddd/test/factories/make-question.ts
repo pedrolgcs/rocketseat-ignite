3716bd89c6137e122d9307930285f83513ed5e74
Pedro Henrique <pedro.lg.cs@gmail.com>
2023-08-08 13:21:52 -0300

    feat: add faker values in question factory

--- 2023/backend/module_04/clean-ddd/test/factories/make-question.ts
+++ 2023/backend/module_04/clean-ddd/test/factories/make-question.ts
@@ -1,13 +1,20 @@
+import { faker } from '@faker-js/faker'
 import { UniqueEntityID } from '@/core/entities/unique-entity-id'
 import { Question, QuestionProps } from '@/domain/forum/enterprise/entities'
 
-export function makeQuestion(override: Partial<QuestionProps> = {}) {
-  const question = Question.create({
-    authorId: new UniqueEntityID(),
-    title: 'example question',
-    content: 'example question',
-    ...override,
-  })
+export function makeQuestion(
+  override: Partial<QuestionProps> = {},
+  id?: UniqueEntityID,
+) {
+  const question = Question.create(
+    {
+      authorId: new UniqueEntityID(),
+      title: faker.lorem.sentence(),
+      content: faker.lorem.text(),
+      ...override,
+    },
+    id,
+  )
 
   return question
 }
