74cb8bf0a94b0158ef715006c9ac7af21aeac7e2
Pedro Henrique <pedro.lg.cs@gmail.com>
2023-08-25 22:18:46 -0300

    feat: input foward ref

--- 2023/frontend/projetos/masterizando-o-tailwind/src/components/ui/Input/Control.tsx
+++ 2023/frontend/projetos/masterizando-o-tailwind/src/components/ui/Input/Control.tsx
@@ -1,18 +1,22 @@
+import * as React from 'react'
 import { cn } from '@/lib/tw-merge'
 
 type ControlProps = React.ComponentProps<'input'>
 
-function Control(props: ControlProps) {
+function Control(props: ControlProps, ref: React.Ref<HTMLInputElement>) {
   return (
     <input
       className={cn(
         'w-full border-0 bg-transparent p-0 text-zinc-900 placeholder-zinc-600',
         props.className,
       )}
+      ref={ref}
       {...props}
     />
   )
 }
 
-export { Control }
+const ControlWithRef = React.forwardRef(Control)
+
+export { ControlWithRef as Control }
 export type { ControlProps }
