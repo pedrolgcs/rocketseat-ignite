5a5cc6c467d158d594971a1b921d6f3fecd839d9
Pedro Henrique <pedro.lg.cs@gmail.com>
2023-09-01 09:15:35 -0300

    feat: remove country select

--- 2023/frontend/projetos/masterizando-o-tailwind/tailwind.config.ts
+++ 2023/frontend/projetos/masterizando-o-tailwind/tailwind.config.ts
@@ -9,14 +9,17 @@ const config: Config = {
           25: '#fcfaff',
         },
       },
+
       gridTemplateColumns: {
         app: 'minmax(18rem, 20rem) 1fr',
         profile: 'min-content 1fr min-content',
         form: 'minmax(7.5rem, 17.5rem) minmax(25rem, 1fr) minmax(0, 15rem)',
       },
+
       borderWidth: {
         6: '6px',
       },
+
       keyframes: {
         slideDown: {
           from: { height: '0' },
@@ -26,10 +29,16 @@ const config: Config = {
           from: { height: 'var(--radix-accordion-content-height)' },
           to: { height: '0' },
         },
+        slideDownAndFade: {
+          from: { opacity: '0', transform: 'translateY(-2px)' },
+          to: { opacity: '1' },
+        },
       },
+
       animation: {
         slideDown: 'slideDown 300ms cubic-bezier(0.87, 0, 0.13, 1)',
         slideUp: 'slideUp 300ms cubic-bezier(0.87, 0, 0.13, 1)',
+        slideDownAndFade: 'slideDownAndFade 0.4s cubic-bezier(0.16, 1, 0.3, 1)',
       },
     },
   },
