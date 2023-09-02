0e5b58bcc4f742fb010b936121ce7e0e76a1ac41
Pedro Henrique <pedro.lg.cs@gmail.com>
2023-09-02 15:39:35 -0300

    feat: change color on menu is selected

--- 2023/frontend/projetos/masterizando-o-tailwind/src/components/structure/Sidebar/NavGroup/Item.tsx
+++ 2023/frontend/projetos/masterizando-o-tailwind/src/components/structure/Sidebar/NavGroup/Item.tsx
@@ -1,5 +1,17 @@
 import * as React from 'react'
 import Link from 'next/link'
+import { usePathname } from 'next/navigation'
+import { tv } from 'tailwind-variants'
+
+const item = tv({
+  base: 'block px-3 py-2 font-medium text-zinc-600 transition hover:bg-zinc-100 hover:text-zinc-800',
+
+  variants: {
+    variant: {
+      active: 'text-violet-500 hover:text-violet-700',
+    },
+  },
+})
 
 type ItemProps = {
   children: React.ReactNode
@@ -7,11 +19,12 @@ type ItemProps = {
 }
 
 function Item({ href, children }: ItemProps) {
+  const pathname = usePathname()
+
+  const variant = pathname === href ? 'active' : undefined
+
   return (
-    <Link
-      href={href}
-      className="block px-3 py-2 font-medium text-zinc-600 transition hover:bg-zinc-100 hover:text-zinc-800"
-    >
+    <Link href={href} className={item({ variant })}>
       {children}
     </Link>
   )
--- 2023/frontend/projetos/masterizando-o-tailwind/src/components/structure/Sidebar/NavItem/index.tsx
+++ 2023/frontend/projetos/masterizando-o-tailwind/src/components/structure/Sidebar/NavItem/index.tsx
@@ -1,5 +1,23 @@
 import * as React from 'react'
 import Link from 'next/link'
+import { usePathname } from 'next/navigation'
+import { tv } from 'tailwind-variants'
+
+const navItem = tv({
+  slots: {
+    text: 'font-medium text-zinc-700 transition group-hover:text-violet-500',
+    icon: 'h-5 w-5 text-zinc-500',
+  },
+
+  variants: {
+    variant: {
+      active: {
+        text: 'text-violet-500 group-hover:text-violet-700',
+        icon: 'text-violet-500 group-hover:text-violet-700',
+      },
+    },
+  },
+})
 
 type NavItemProps = {
   href: string
@@ -8,16 +26,20 @@ type NavItemProps = {
 }
 
 function NavItem({ href, icon: Icon, children }: NavItemProps) {
+  const pathname = usePathname()
+
+  const variant = pathname === href ? 'active' : undefined
+
+  const slots = navItem({ variant })
+
   return (
     <Link
       href={href}
       className="group flex items-center gap-3 rounded px-3 py-2 transition hover:bg-violet-50"
     >
-      {Icon && <Icon className="h-5 w-5 text-zinc-500" />}
+      {Icon && <Icon className={slots.icon()} />}
 
-      <span className="font-medium text-zinc-700 transition group-hover:text-violet-500">
-        {children}
-      </span>
+      <span className={slots.text()}>{children}</span>
     </Link>
   )
 }
