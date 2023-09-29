4f7ae26b1929503d4cac6301c5ae3a627cc00291
Pedro Henrique <pedro.lg.cs@gmail.com>
2023-09-29 13:51:56 -0300

    feat: configure envoriment variables

--- 2023/backend/module_05/nest-clean/src/app.module.ts
+++ 2023/backend/module_05/nest-clean/src/app.module.ts
@@ -1,8 +1,16 @@
 import { Module } from '@nestjs/common'
+import { ConfigModule } from '@nestjs/config'
 import { CreateAccountController } from './controllers/create-account/create-controller.controller'
+import { envSchema } from './env'
 import { PrismaService } from './prisma/prisma.service'
 
 @Module({
+  imports: [
+    ConfigModule.forRoot({
+      validate: (env) => envSchema.parse(env),
+      isGlobal: true,
+    }),
+  ],
   controllers: [CreateAccountController],
   providers: [PrismaService],
 })
