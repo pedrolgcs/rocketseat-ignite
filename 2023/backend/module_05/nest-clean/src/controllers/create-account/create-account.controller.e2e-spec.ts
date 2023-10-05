d8d9d5d93cb14cb0e667e6213ac84682646c24c9
Pedro Henrique <pedro.lg.cs@gmail.com>
2023-10-05 09:54:38 -0300

    chore: configure vite and swc

---
+++ 2023/backend/module_05/nest-clean/src/controllers/create-account/create-account.controller.e2e-spec.ts
@@ -0,0 +1,5 @@
+import { expect, test } from 'vitest'
+
+test('1 plus 1', () => {
+  expect(1 + 1).toBe(2)
+})
---
+++ 2023/backend/module_05/nest-clean/vitest.config.e2e.ts
@@ -0,0 +1,18 @@
+import swc from 'unplugin-swc'
+import tsConfigPaths from 'vite-tsconfig-paths'
+import { defineConfig } from 'vitest/config'
+
+export default defineConfig({
+  test: {
+    exclude: ['data', 'node_modules'],
+    include: ['**/*.e2e-spec.ts'],
+    globals: true,
+    root: './',
+  },
+  plugins: [
+    tsConfigPaths(),
+    swc.vite({
+      module: { type: 'es6' },
+    }),
+  ],
+})
