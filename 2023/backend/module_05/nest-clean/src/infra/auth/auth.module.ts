4efd045f191754645d4472997a27e4e5522c7a32
Pedro Henrique <pedro.lg.cs@gmail.com>
2023-10-14 14:08:59 -0300

    feat: create env module

--- 2023/backend/module_05/nest-clean/src/infra/auth/auth.module.ts
+++ 2023/backend/module_05/nest-clean/src/infra/auth/auth.module.ts
@@ -1,21 +1,23 @@
 import { Module } from '@nestjs/common'
-import { ConfigService } from '@nestjs/config'
 import { APP_GUARD } from '@nestjs/core'
 import { JwtModule } from '@nestjs/jwt'
 import { PassportModule } from '@nestjs/passport'
-import { Env } from '@/infra/env'
+import { EnvModule } from '../env/env.module'
+import { EnvService } from '../env/env.service'
 import { JwtAuthGuard } from './jwt-auth.guard'
 import { JwtStrategy } from './jwt.strategy'
 
 @Module({
   imports: [
+    EnvModule,
     PassportModule,
     JwtModule.registerAsync({
-      inject: [ConfigService],
+      imports: [EnvModule],
+      inject: [EnvService],
       global: true,
-      useFactory(config: ConfigService<Env, true>) {
-        const privateKey = config.get('JWT_PRIVATE_KEY', { infer: true })
-        const publicKey = config.get('JWT_PUBLIC_KEY', { infer: true })
+      useFactory(env: EnvService) {
+        const privateKey = env.get('JWT_PRIVATE_KEY')
+        const publicKey = env.get('JWT_PUBLIC_KEY')
 
         return {
           signOptions: {
