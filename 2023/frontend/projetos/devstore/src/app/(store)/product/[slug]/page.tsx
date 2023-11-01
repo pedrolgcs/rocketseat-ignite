72939fd51b92e3f607f45a70f46be30ae01e4d7d
Pedro Henrique <pedro.lg.cs@gmail.com>
2023-11-01 13:00:42 -0300

    refactor: mobile justify

--- 2023/frontend/projetos/devstore/src/app/(store)/product/[slug]/page.tsx
+++ 2023/frontend/projetos/devstore/src/app/(store)/product/[slug]/page.tsx
@@ -47,70 +47,74 @@ export default async function ProductPage({ params }: ProductPage) {
         />
       </div>
 
-      <div className={cn('flex flex-1 flex-col justify-center', 'lg:px-12')}>
-        <h1 className="text-3xl font-bold leading-tight">{product.title}</h1>
+      <div
+        className={cn(
+          'flex flex-1 flex-col justify-between',
+          'lg:justify-center lg:px-12',
+        )}
+      >
+        <div className="flex flex-col">
+          <h1 className="text-3xl font-bold leading-tight">{product.title}</h1>
 
-        <p className="mt-2 leading-relaxed text-zinc-400">
-          {product.description}
-        </p>
+          <p className="mt-2 leading-relaxed text-zinc-400">
+            {product.description}
+          </p>
 
-        <div className={cn('mt-5 flex items-center gap-3', 'lg:mt-8')}>
-          <span className="flex items-center justify-center rounded-full bg-violet-500 px-5 py-2.5 font-semibold">
-            {product.price.toLocaleString('pt-BR', {
-              style: 'currency',
-              currency: 'BRL',
-            })}
-          </span>
+          <div className={cn('mt-5 flex items-center gap-3', 'lg:mt-8')}>
+            <span className="flex items-center justify-center rounded-full bg-violet-500 px-5 py-2.5 font-semibold">
+              {product.price.toLocaleString('pt-BR', {
+                style: 'currency',
+                currency: 'BRL',
+              })}
+            </span>
 
-          <span className="text-sm text-zinc-400">
-            Em 12x s/ juros de R${' '}
-            {(product.price / 12).toLocaleString('pt-BR', {
-              style: 'currency',
-              currency: 'BRL',
-            })}
-          </span>
-        </div>
+            <span className="text-sm text-zinc-400">
+              Em 12x s/ juros de R${' '}
+              {(product.price / 12).toLocaleString('pt-BR', {
+                style: 'currency',
+                currency: 'BRL',
+              })}
+            </span>
+          </div>
 
-        <div className={cn('mt-5 space-y-4 pb-5', 'lg:mt-8 lg:p-0')}>
-          <span className="block text-2xl font-semibold">Tamanhos</span>
+          <div className={cn('mt-5 space-y-4', 'lg:mt-8')}>
+            <span className="block text-2xl font-semibold">Tamanhos</span>
 
-          <div className="flex gap-2">
-            <button
-              type="button"
-              className="flex h-9 w-14 items-center justify-center rounded-full border border-zinc-700 bg-zinc-800 text-sm font-semibold transition-colors duration-300 hover:border-zinc-600 hover:bg-zinc-700"
-            >
-              P
-            </button>
+            <div className="flex gap-2">
+              <button
+                type="button"
+                className="flex h-9 w-14 items-center justify-center rounded-full border border-zinc-700 bg-zinc-800 text-sm font-semibold transition-colors duration-300 hover:border-zinc-600 hover:bg-zinc-700"
+              >
+                P
+              </button>
 
-            <button
-              type="button"
-              className="flex h-9 w-14 items-center justify-center rounded-full border border-zinc-700 bg-zinc-800 text-sm font-semibold transition-colors duration-300 hover:border-zinc-600 hover:bg-zinc-700"
-            >
-              M
-            </button>
+              <button
+                type="button"
+                className="flex h-9 w-14 items-center justify-center rounded-full border border-zinc-700 bg-zinc-800 text-sm font-semibold transition-colors duration-300 hover:border-zinc-600 hover:bg-zinc-700"
+              >
+                M
+              </button>
 
-            <button
-              type="button"
-              className="flex h-9 w-14 items-center justify-center rounded-full border border-zinc-700 bg-zinc-800 text-sm font-semibold transition-colors duration-300 hover:border-zinc-600 hover:bg-zinc-700"
-            >
-              G
-            </button>
+              <button
+                type="button"
+                className="flex h-9 w-14 items-center justify-center rounded-full border border-zinc-700 bg-zinc-800 text-sm font-semibold transition-colors duration-300 hover:border-zinc-600 hover:bg-zinc-700"
+              >
+                G
+              </button>
 
-            <button
-              type="button"
-              className="flex h-9 w-14 items-center justify-center rounded-full border border-zinc-700 bg-zinc-800 text-sm font-semibold duration-300 hover:border-zinc-600 hover:bg-zinc-700"
-            >
-              GG
-            </button>
+              <button
+                type="button"
+                className="flex h-9 w-14 items-center justify-center rounded-full border border-zinc-700 bg-zinc-800 text-sm font-semibold duration-300 hover:border-zinc-600 hover:bg-zinc-700"
+              >
+                GG
+              </button>
+            </div>
           </div>
         </div>
 
         <button
           type="button"
-          className={cn(
-            'mt-auto flex h-12 items-center justify-center rounded-full bg-emerald-600 font-semibold text-white transition-colors duration-300 hover:bg-emerald-700',
-            'lg:mt-8',
-          )}
+          className="mt-8 flex h-12 items-center justify-center rounded-full bg-emerald-600 font-semibold text-white transition-colors duration-300 hover:bg-emerald-700"
         >
           Adicionar ao Carrinho
         </button>
