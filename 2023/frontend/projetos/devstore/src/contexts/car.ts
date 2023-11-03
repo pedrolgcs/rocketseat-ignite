0987e2a45a30859ca779f5b4d861a94f6811c4ff
Pedro Henrique <pedro.lg.cs@gmail.com>
2023-11-03 15:47:23 -0300

    feat: create cart context

---
+++ 2023/frontend/projetos/devstore/src/contexts/car.ts
@@ -0,0 +1,58 @@
+import { create } from 'zustand'
+
+type CartItem = {
+  productId: string
+  quantity: number
+}
+
+type State = {
+  items: CartItem[]
+}
+
+type Actions = {
+  addToCart: (productId: string) => void
+  reset: () => void
+}
+
+type CartContext = State & Actions
+
+const initialState: State = {
+  items: [],
+}
+
+export const useCart = create<CartContext>((set, get) => {
+  return {
+    ...initialState,
+
+    addToCart: (productId: string) => {
+      const { items } = get()
+
+      const productCartIndex = items.findIndex(
+        (item) => item.productId === productId,
+      )
+
+      if (productCartIndex) {
+        items[productCartIndex].quantity++
+
+        return set({
+          items,
+        })
+      } else {
+        const newItem = {
+          productId,
+          quantity: 1,
+        }
+
+        items.push(newItem)
+
+        return set({
+          items,
+        })
+      }
+    },
+
+    reset: () => {
+      set(initialState)
+    },
+  }
+})
