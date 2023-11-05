56b40caadb4d24d6b4c12297a2b499b020183f58
Pedro Henrique <pedro.lg.cs@gmail.com>
2023-11-05 14:50:11 -0300

    feat: create opengraph image to product page

---
+++ 2023/frontend/projetos/devstore/src/app/(store)/product/[slug]/opengraph-image.tsx
@@ -0,0 +1,49 @@
+import Image from 'next/image'
+import { ImageResponse } from 'next/og'
+import colors from 'tailwindcss/colors'
+import { getProduct } from '@/data/request/product'
+import { env } from '@/env'
+
+// Route segment config
+export const runtime = 'edge'
+
+// Image metadata
+export const alt = 'About Acme'
+
+export const size = {
+  width: 1200,
+  height: 630,
+}
+
+export const contentType = 'image/png'
+
+type ImageProps = {
+  params: {
+    slug: string
+  }
+}
+
+export default async function OgImage({ params }: ImageProps) {
+  const product = await getProduct(params.slug)
+
+  const productImageUrl = new URL(product.image, env.APP_URL).toString()
+
+  return new ImageResponse(
+    (
+      <div
+        style={{
+          background: colors.zinc['950'],
+          width: '100%',
+          height: '100%',
+          display: 'flex',
+          flexDirection: 'column',
+        }}
+      >
+        <img src={productImageUrl} alt="" style={{ width: '100%' }} />
+      </div>
+    ),
+    {
+      ...size,
+    },
+  )
+}
