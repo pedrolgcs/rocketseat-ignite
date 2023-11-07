b5eec70c93f500c3b8ed9ddfc7dc2b7701003962
Pedro Henrique <pedro.lg.cs@gmail.com>
2023-11-07 14:01:54 -0300

    feat: create loading page to search products

--- 2023/frontend/projetos/devstore/src/app/(store)/search/loading.tsx
+++ 2023/frontend/projetos/devstore/src/app/(store)/search/loading.tsx
@@ -1,3 +1,25 @@
+'use client'
+
+import { useSearchParams } from 'next/navigation'
+import { Skeleton } from '@/components'
+import { cn } from '@/lib/tw-merge'
+
 export default function Loading() {
-  return <p>carregando busca...</p>
+  const searchParams = useSearchParams()
+
+  const query = searchParams.get('q')
+
+  return (
+    <div className="flex flex-col gap-4">
+      <p className="text-sm">
+        Resultados para: <span className="font-semibold"> {query} </span>
+      </p>
+
+      <div className={cn('flex flex-col gap-6', 'lg:grid lg:grid-cols-3')}>
+        {Array.from({ length: 3 }).map((_, index) => (
+          <Skeleton key={index} className={cn('flex h-72', 'lg:h-80')} />
+        ))}
+      </div>
+    </div>
+  )
 }
