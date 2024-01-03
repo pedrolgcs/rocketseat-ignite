1fdf312fcf45dcbcb2e4c9850f9eef6ccb082648
Pedro Henrique <pedro.lg.cs@gmail.com>
2024-01-03 15:03:04 -0300

    feat: add tooltip in orders chart

--- 2023/frontend/projetos/pizza-shop/src/features/dashboard/components/revenue-chart.tsx
+++ 2023/frontend/projetos/pizza-shop/src/features/dashboard/components/revenue-chart.tsx
@@ -1,3 +1,4 @@
+import { format } from 'date-fns'
 import {
   CartesianGrid,
   Line,
@@ -9,6 +10,7 @@ import {
 } from 'recharts'
 import colors from 'tailwindcss/colors'
 
+import { useTheme } from '@/components/theme/theme-provider'
 import {
   Card,
   CardContent,
@@ -16,39 +18,42 @@ import {
   CardHeader,
   CardTitle,
 } from '@/components/ui/card'
+import { formatCurrency } from '@/utils/formatCurrency'
 
 const data = [
   {
-    date: '10/12',
+    date: new Date('2024-01-01'),
     revenue: 1200,
   },
   {
-    date: '11/12',
+    date: new Date('2024-01-02'),
     revenue: 200,
   },
   {
-    date: '12/12',
+    date: new Date('2024-01-03'),
     revenue: 900,
   },
   {
-    date: '13/12',
+    date: new Date('2024-01-04'),
     revenue: 750,
   },
   {
-    date: '14/12',
+    date: new Date('2024-01-05'),
     revenue: 640,
   },
   {
-    date: '15/12',
+    date: new Date('2024-01-06'),
     revenue: 250,
   },
   {
-    date: '16/12',
+    date: new Date('2024-01-07'),
     revenue: 430,
   },
 ]
 
 export function RevenueChart() {
+  const { theme } = useTheme()
+
   return (
     <Card className="col-span-6">
       <CardHeader className="flex-row items-center justify-between pb-8">
@@ -63,20 +68,33 @@ export function RevenueChart() {
       <CardContent>
         <ResponsiveContainer width="100%" height={240}>
           <LineChart data={data} style={{ fontSize: 12 }}>
-            <XAxis dataKey="date" tickLine={false} axisLine={false} dy={16} />
+            <Tooltip
+              labelFormatter={(value: Date) => format(value, 'E dd MMM y')}
+              formatter={(value: number) => formatCurrency(value)}
+              itemStyle={{ fontSize: 16 }}
+              contentStyle={{
+                backgroundColor:
+                  theme === 'dark' ? colors.zinc[800] : colors.zinc[50],
+              }}
+            />
+
+            <XAxis
+              dataKey="date"
+              tickLine={false}
+              axisLine={false}
+              dy={16}
+              tickFormatter={(value: Date) => format(value, 'dd/MM')}
+            />
 
             <YAxis
               stroke="#888"
               axisLine={false}
               tickLine={false}
-              width={100}
-              tickFormatter={(value: number) =>
-                value.toLocaleString('pt-BR', {
-                  style: 'currency',
-                  currency: 'BRL',
-                })
-              }
+              width={80}
+              tickFormatter={(value: number) => formatCurrency(value)}
             />
+
+            <CartesianGrid vertical={false} className="stroke-muted" />
 
             <Line
               type="linear"
