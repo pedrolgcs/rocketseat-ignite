967260cd85d909777809d72d6b90ea20964cea7d
Pedro Henrique <pedro.lg.cs@gmail.com>
2024-01-30 13:46:48 -0300

    feat: update orders list on cancel order

--- 2023/frontend/projetos/pizza-shop/src/features/orders-table/hooks/useCancelOrderMutation.ts
+++ 2023/frontend/projetos/pizza-shop/src/features/orders-table/hooks/useCancelOrderMutation.ts
@@ -1,9 +1,38 @@
-import { useMutation } from '@tanstack/react-query'
+import { useMutation, useQueryClient } from '@tanstack/react-query'
 
 import { cancelOrder, CancelOrderParams } from '../api/cancel-order'
+import { GetOrdersResponse } from '../api/get-orders'
+import { USE_GET_ORDERS_QUERY_KEY } from './useGetOrdersQuery'
 
 export const useCancelOrderMutation = () => {
+  const queryClient = useQueryClient()
+
   return useMutation({
     mutationFn: (params: CancelOrderParams) => cancelOrder(params),
+    onSuccess(_, variables) {
+      const { orderId } = variables
+      const ordersListCache = queryClient.getQueriesData<GetOrdersResponse>({
+        queryKey: [USE_GET_ORDERS_QUERY_KEY],
+      })
+
+      ordersListCache.forEach((cached) => {
+        const [key, data] = cached
+        if (!data) return null
+
+        queryClient.setQueryData<GetOrdersResponse>(key, {
+          ...data,
+          orders: data.orders.map((order) => {
+            if (order.orderId === orderId) {
+              return {
+                ...order,
+                status: 'canceled',
+              }
+            }
+
+            return order
+          }),
+        })
+      })
+    },
   })
 }
