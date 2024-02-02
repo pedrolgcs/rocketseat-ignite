139f59a25f420e48114556aa27e7cbf3d00a2ca4
Pedro Henrique <pedro.lg.cs@gmail.com>
2024-02-02 14:55:24 -0300

    chore: update query client options

--- 2023/frontend/projetos/pizza-shop/src/lib/react-query.ts
+++ 2023/frontend/projetos/pizza-shop/src/lib/react-query.ts
@@ -1,13 +1,3 @@
 import { QueryClient } from '@tanstack/react-query'
 
-export const queryClient = new QueryClient({
-  defaultOptions: {
-    queries: {
-      refetchOnWindowFocus: false,
-      retry: 1,
-    },
-    mutations: {
-      retry: 1,
-    },
-  },
-})
+export const queryClient = new QueryClient()
