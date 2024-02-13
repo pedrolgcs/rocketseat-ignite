19a8c49ecd4d11b6ee3bf4e1fc23e0be72e77f93
Pedro Henrique <pedro.lg.cs@gmail.com>
2024-02-13 14:30:59 -0300

    test: create sign up e2e tests

---
+++ 2023/frontend/projetos/pizza-shop/test/e2e/sign-up.e2e-spec.ts
@@ -0,0 +1,41 @@
+import { expect, test } from '@playwright/test'
+
+test('[E2E] SignUp - Success', async ({ page }) => {
+  await page.goto('/sign-up', { waitUntil: 'networkidle' })
+
+  await page.getByLabel('Nome do estabelecimento').fill('Pizza Shop')
+  await page.getByLabel('Seu nome').fill('John Doe')
+  await page.getByLabel('Seu e-mail').fill('johndoe@example.com')
+  await page.getByLabel('Seu celular').fill('11999999999')
+
+  await page.getByRole('button', { name: 'Finalizar cadastro' }).click()
+
+  const toast = page.getByText('Restaurante cadastrado com sucesso!')
+
+  expect(toast).toBeVisible()
+  await page.waitForTimeout(1000)
+})
+
+test('[E2E] SignUp - Error', async ({ page }) => {
+  await page.goto('/sign-up', { waitUntil: 'networkidle' })
+
+  await page.getByLabel('Nome do estabelecimento').fill('wrong name')
+  await page.getByLabel('Seu nome').fill('John Doe')
+  await page.getByLabel('Seu e-mail').fill('johndoe@example.com')
+  await page.getByLabel('Seu celular').fill('11999999999')
+
+  await page.getByRole('button', { name: 'Finalizar cadastro' }).click()
+
+  const toast = page.getByText('Erro ao cadastrar restaurante.')
+
+  expect(toast).toBeVisible()
+  await page.waitForTimeout(1000)
+})
+
+test('[E2E] SignUp - Navigate to sign in', async ({ page }) => {
+  await page.goto('/sign-up', { waitUntil: 'networkidle' })
+
+  await page.getByRole('link', { name: 'Fazer login' }).click()
+
+  expect(page.url()).toContain('/sign-in')
+})
