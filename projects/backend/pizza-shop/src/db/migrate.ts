892ed4deb0c37f455babe37da9934e70334e9dc7
Pedro Henrique <pedro.lg.cs@gmail.com>
2024-02-19 15:00:54 -0300

    feat: configure env

--- projects/backend/pizza-shop/src/db/migrate.ts
+++ projects/backend/pizza-shop/src/db/migrate.ts
@@ -1,15 +1,18 @@
+import chalk from 'chalk'
 import { drizzle } from 'drizzle-orm/postgres-js'
 import { migrate } from 'drizzle-orm/postgres-js/migrator'
 import postgres from 'postgres'
 
-const connection = postgres(
-  'postgressql://docker:docker@localhost:5432/pizzashop',
-  { max: 1 },
-)
+import { env } from '../env'
+
+const connection = postgres(env.DATABASE_URL, { max: 1 })
 
 const db = drizzle(connection)
 
 await migrate(db, { migrationsFolder: 'drizzle' })
+
+console.log(chalk.green('Database migrated successfully'))
+
 await connection.end()
 
 process.exit()
