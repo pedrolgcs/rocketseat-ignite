fc0f321955d03a93843faa2218b541f942e49489
Pedro Henrique <pedro.lg.cs@gmail.com>
2024-02-27 15:36:33 -0300

    feat: create auth links table

---
+++ projects/backend/pizza-shop/src/infra/db/schema/auth-links.ts
@@ -0,0 +1,15 @@
+import { createId } from '@paralleldrive/cuid2'
+import { pgTable, text, timestamp } from 'drizzle-orm/pg-core'
+
+import { users } from './users'
+
+export const authLinks = pgTable('aith_links', {
+  id: text('id')
+    .$defaultFn(() => createId())
+    .primaryKey(),
+  code: text('code').notNull().unique(),
+  userId: text('user_id')
+    .references(() => users.id)
+    .notNull(),
+  created_at: timestamp('created_at').notNull().defaultNow(),
+})
