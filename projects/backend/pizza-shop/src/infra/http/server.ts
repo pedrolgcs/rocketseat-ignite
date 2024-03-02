e4118c3aae9fd373e20306f9837b2013a9faf8de
Pedro Henrique <pedro.lg.cs@gmail.com>
2024-03-02 14:20:44 -0300

    chore: add jwt and cookie plugins

--- projects/backend/pizza-shop/src/infra/http/server.ts
+++ projects/backend/pizza-shop/src/infra/http/server.ts
@@ -1,10 +1,22 @@
-import { Elysia } from 'elysia'
+import cookie from '@elysiajs/cookie'
+import jwt from '@elysiajs/jwt'
+import { Elysia, t } from 'elysia'
 
+import { env } from '../env'
 import { routes } from './routes'
 
 const app = new Elysia()
-
-app.use(routes)
+  .use(
+    jwt({
+      secret: env.JWT_SECRET_KEY,
+      schema: t.Object({
+        sub: t.String(),
+        restaurantId: t.Optional(t.String()),
+      }),
+    }),
+  )
+  .use(cookie)
+  .use(routes)
 
 app.listen(3333, () => {
   console.log('🚀 Listening on http://localhost:3333')
