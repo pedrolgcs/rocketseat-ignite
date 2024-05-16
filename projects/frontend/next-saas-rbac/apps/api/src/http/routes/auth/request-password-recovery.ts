158fd6e4af7500001d2f5bd5249f271787e8f8ae
Pedro Henrique <pedro.lg.cs@gmail.com>
2024-05-16 14:51:30 -0300

    feat: request password recovery and reset password

---
+++ projects/frontend/next-saas-rbac/apps/api/src/http/routes/auth/request-password-recovery.ts
@@ -0,0 +1,49 @@
+import type { FastifyInstance } from 'fastify'
+import type { ZodTypeProvider } from 'fastify-type-provider-zod'
+import z from 'zod'
+
+import { prisma } from '@/lib/prisma'
+
+const bodySchema = z.object({
+  email: z.string().email(),
+})
+
+export async function requestPasswordRecovery(app: FastifyInstance) {
+  app.withTypeProvider<ZodTypeProvider>().post(
+    '/password/recover',
+    {
+      schema: {
+        tags: ['Auth'],
+        summary: 'Request password recovery',
+        body: bodySchema,
+        response: {
+          201: z.null(),
+        },
+      },
+    },
+    async (request, reply) => {
+      const { email } = request.body
+
+      const userFromEmail = await prisma.user.findUnique({
+        where: {
+          email,
+        },
+      })
+
+      if (!userFromEmail) {
+        return reply.status(201).send()
+      }
+
+      const { id: code } = await prisma.token.create({
+        data: {
+          type: 'PASSWORD_RECOVER',
+          userId: userFromEmail.id,
+        },
+      })
+
+      console.log('code', code)
+
+      return reply.status(201).send()
+    },
+  )
+}
--- projects/frontend/next-saas-rbac/apps/api/src/http/routes/index.ts
+++ projects/frontend/next-saas-rbac/apps/api/src/http/routes/index.ts
@@ -3,10 +3,13 @@ import type { FastifyInstance } from 'fastify'
 import { authenticateWithPassword } from './auth/authenticate-with-password'
 import { createAccount } from './auth/create-account'
 import { getProfile } from './auth/get-profile'
+import { requestPasswordRecovery } from './auth/request-password-recovery'
+import { resetPassword } from './auth/reset-password'
 
 export async function routes(app: FastifyInstance) {
   createAccount(app)
   authenticateWithPassword(app)
-
   getProfile(app)
+  requestPasswordRecovery(app)
+  resetPassword(app)
 }
