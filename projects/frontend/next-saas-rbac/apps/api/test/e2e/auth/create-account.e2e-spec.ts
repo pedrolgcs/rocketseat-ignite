59893ccd9f5db575a8288ef1a9d5d5be41716857
Pedro Henrique <pedro.lg.cs@gmail.com>
2024-06-06 12:36:37 -0300

    test: rename to create account

--- projects/frontend/next-saas-rbac/apps/api/test/e2e/auth/create-account.e2e-spec.ts
+++ projects/frontend/next-saas-rbac/apps/api/test/e2e/auth/create-account.e2e-spec.ts
@@ -3,7 +3,7 @@ import { afterAll, beforeAll, describe, expect, it } from 'vitest'
 
 import { app } from '@/http/app'
 
-describe('[Auth] - Create User', () => {
+describe('[Auth] - Create Account', () => {
   beforeAll(async () => {
     await app.ready()
   })
@@ -12,7 +12,7 @@ describe('[Auth] - Create User', () => {
     await app.close()
   })
 
-  it('should be able to create a new user', async () => {
+  it('should be able to create a new account', async () => {
     const response = await request(app.server).post('/users').send({
       name: 'Pedro Henrique',
       email: 'pedro@gmail.com',
