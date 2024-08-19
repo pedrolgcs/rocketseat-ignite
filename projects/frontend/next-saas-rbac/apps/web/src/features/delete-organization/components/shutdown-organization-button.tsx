5a7dc9f72dafbcb27d46fddebf4715638ba603bb
Pedro Henrique <pedro.lg.cs@gmail.com>
2024-08-19 14:40:35 -0300

    feat: alert when click in delete organization

--- projects/frontend/next-saas-rbac/apps/web/src/features/delete-organization/components/shutdown-organization-button.tsx
+++ projects/frontend/next-saas-rbac/apps/web/src/features/delete-organization/components/shutdown-organization-button.tsx
@@ -4,6 +4,17 @@ import { XCircleIcon } from 'lucide-react'
 import { useRouter } from 'next/navigation'
 
 import { Alert, AlertDescription, AlertTitle } from '@/components/ui/alert'
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from '@/components/ui/alert-dialog'
 import { Button } from '@/components/ui/button'
 import { useGetCurrentOrganization } from '@/hooks/use-get-current-organization'
 
@@ -38,16 +49,34 @@ export function ShutdownOrganizationButton() {
         </Alert>
       )}
 
-      <Button
-        type="button"
-        variant="destructive"
-        className="w-56"
-        onClick={handleShutdownOrganization}
-        disabled={isPending}
-      >
-        <XCircleIcon className="mr-2 size-4" />
-        Shutdown organization
-      </Button>
+      <AlertDialog>
+        <AlertDialogTrigger asChild>
+          <Button type="button" variant="destructive" className="w-56">
+            <XCircleIcon className="mr-2 size-4" />
+            Shutdown organization
+          </Button>
+        </AlertDialogTrigger>
+
+        <AlertDialogContent>
+          <AlertDialogHeader>
+            <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
+            <AlertDialogDescription>
+              This action cannot be undone. This will permanently delete your
+              organization and remove your data from our servers.
+            </AlertDialogDescription>
+          </AlertDialogHeader>
+
+          <AlertDialogFooter>
+            <AlertDialogCancel>Cancel</AlertDialogCancel>
+            <AlertDialogAction
+              onClick={handleShutdownOrganization}
+              disabled={isPending}
+            >
+              Continue
+            </AlertDialogAction>
+          </AlertDialogFooter>
+        </AlertDialogContent>
+      </AlertDialog>
     </div>
   )
 }
