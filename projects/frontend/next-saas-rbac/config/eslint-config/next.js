faabb289e9de89039fec259a91ffdbba92239456
Pedro Henrique <pedro.lg.cs@gmail.com>
2024-09-21 13:07:46 -0300

    feat: add eslint plugin hooks

--- projects/frontend/next-saas-rbac/config/eslint-config/next.js
+++ projects/frontend/next-saas-rbac/config/eslint-config/next.js
@@ -1,6 +1,6 @@
 /** @type{import('eslint').Linter.Config} */
 module.exports = {
-  extends: ['@rocketseat/eslint-config/next'],
+  extends: ['@rocketseat/eslint-config/next', 'plugin:react-hooks/recommended'],
   plugins: ['simple-import-sort'],
   rules: {
     'simple-import-sort/imports': 'error',
