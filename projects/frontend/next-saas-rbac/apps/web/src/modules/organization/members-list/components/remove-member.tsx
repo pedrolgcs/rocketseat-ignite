fa7355cf0ab4d48558bbef027c98d70077c1c8d0
Pedro Henrique <pedro.lg.cs@gmail.com>
2024-09-23 09:14:41 -0300

    feat: add confirm dialog in remove member

--- projects/frontend/next-saas-rbac/apps/web/src/modules/organization/members-list/components/remove-member.tsx
+++ projects/frontend/next-saas-rbac/apps/web/src/modules/organization/members-list/components/remove-member.tsx
@@ -1,6 +1,17 @@
 import { Loader2Icon, UserMinusIcon } from 'lucide-react'
 import { toast } from 'sonner'
 
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from '@/components/ui/alert-dialog'
 import { Button } from '@/components/ui/button'
 import { useRemoveMemberMutation } from '@/http/hooks/use-remove-member-mutation'
 
@@ -44,14 +55,33 @@ export function RemoveMember({
   }
 
   return (
-    <Button
-      size="sm"
-      variant="destructive"
-      onClick={handleRemoveMember}
-      disabled={me === member.userId}
-    >
-      <UserMinusIcon className="mr-2 size-4" />
-      Remove
-    </Button>
+    <AlertDialog>
+      <AlertDialogTrigger asChild>
+        <Button size="sm" variant="destructive" disabled={me === member.userId}>
+          <UserMinusIcon className="mr-2 size-4" />
+          Remove
+        </Button>
+      </AlertDialogTrigger>
+
+      <AlertDialogContent>
+        <AlertDialogHeader>
+          <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
+          <AlertDialogDescription>
+            This action cannot be undone. This will permanently remove member
+            from the organization.
+          </AlertDialogDescription>
+        </AlertDialogHeader>
+
+        <AlertDialogFooter>
+          <AlertDialogCancel>Cancel</AlertDialogCancel>
+          <AlertDialogAction
+            onClick={handleRemoveMember}
+            disabled={isPendingOnRemoveMember}
+          >
+            Continue
+          </AlertDialogAction>
+        </AlertDialogFooter>
+      </AlertDialogContent>
+    </AlertDialog>
   )
 }
