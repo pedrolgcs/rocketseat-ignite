67023f202fa3172c8023e20ce4e6f2b5edac129a
Pedro Henrique <pedro.lg.cs@gmail.com>
2024-10-19 23:31:51 -0300

    chore: set host

--- projects/frontend/next-saas-rbac/apps/api/src/http/server.ts
+++ projects/frontend/next-saas-rbac/apps/api/src/http/server.ts
@@ -4,7 +4,7 @@ import { removeOldTokensJob } from '@/jobs/remove-old-tokens'
 
 import { app } from './app'
 
-app.listen({ port: env.PORT }).then(() => {
+app.listen({ port: env.PORT, host: '0.0.0.0' }).then(() => {
   removeOldTokensJob.start()
 
   console.log('🚀 HTTP Server running on http://localhost:3333')
